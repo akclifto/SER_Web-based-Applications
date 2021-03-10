d9c352eead6a6519558412de68d731899f74e60c
akclifto <akclifto@asu.edu>
2021-03-09 17:32:59 -0700

    work on partA

--- Lab1/src/lab1act2partA_test.js
+++ Lab1/src/lab1act2partA_test.js
@@ -6,3 +6,26 @@
  * @date 2021.03.09
  * 
  */
+
+let result = 0;
+
+function calc(String) {
+    let  calc = JSON.parse(String);
+
+    if(calc.op === "add" ) {
+        // console.log("add");
+        result += calc.number;
+    } 
+    else if (calc.op === "subtract") {
+        // console.log("subtract");
+        result -= calc.number;
+
+    } else {
+        console.log("some default");
+    }
+    return result;
+};
+
+console.log(calc('{"op" : "add", "number" : 5}')); //5
+console.log(calc('{"op" : "subtract", "number" : 2}')); // 3
+console.log(calc('{"op" : "add", "number" : 19}')); // 22
