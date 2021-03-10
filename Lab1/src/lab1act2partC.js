b0d79a51d33d3e1cccf02902f0d8adc8c2d596cd
akclifto <akclifto@asu.edu>
2021-03-10 13:05:11 -0700

    push pop print work, need to integrate

--- Lab1/src/lab1act2partC.js
+++ Lab1/src/lab1act2partC.js
@@ -7,3 +7,95 @@
  * 
  */
 
+class PreCalc {
+    constructor(number) {
+        this.calcStack = [number];
+        this.result = 0;
+    };
+};
+
+PreCalc.prototype.calc = function (String) {
+
+    let calcStack = this.calcStack;
+    let result = this.calcStack[0];
+
+    let calc = JSON.parse(String);
+    if (calc.expr) {
+
+        let op = []
+        let lastExpression = calc;
+        let nextExpression = calc.expr;
+
+        op.unshift(calc.op);
+
+        while (nextExpression) {
+            lastExpression = nextExpression;
+            op.unshift(lastExpression.op); 
+            nextExpression = nextExpression.expr 
+        }
+        for (let i in op) {
+            if (op[i] === "add") {
+                result += lastExpression.number;
+                lastExpression.number = result;
+            }
+            else if (op[i] === "subtract") {
+                result -= lastExpression.number;
+                lastExpression.number = result;
+            }
+            else {
+                getErrorMessage();
+            }
+        }
+    } else {
+        result = this.doMaths(calc);
+    }
+
+    return result;
+
+};
+
+PreCalc.prototype.push = function (result) {
+    // push value to front
+    this.calcStack.unshift(result);
+};
+
+PreCalc.prototype.pop = function () {
+    // remove value in front
+    const holder = this.calcStack.shift();
+    return holder;
+};
+
+PreCalc.prototype.print = function () {
+    return "[ " + this.calcStack.toString() + " ]";
+};
+
+PreCalc.prototype.doMaths = function (maths) {
+    console.log("domaths");
+    console.log(maths);
+
+    if (maths.op === "add") {
+        this.result += maths.number;
+    }
+    else if (maths.op === "subtract") {
+        this.result -= maths.number;
+    } else {
+        getErrorMessage();
+    }
+    return this.result;
+};
+
+PreCalc.prototype.exec = function (array) {
+    for (let i in array) {
+        let op = JSON.stringify(array[i].exp);
+        this.result = calc(op);
+        console.log(this.result + " = " + array[i].expected);
+    }
+    return "exec() complete."
+};
+
+PreCalc.prototype.cleanup = function () {
+    this.result = 0;
+    return "result reset to: " + this.result;
+};
+
+export default PreCalc;
--- Lab1/src/lab1act2partC_test.js
+++ Lab1/src/lab1act2partC_test.js
@@ -6,3 +6,20 @@
  * @date 2021.03.10
  * 
  */
+import PreCalc from "./lab1act2partC.js";
+
+let pc = new PreCalc(0);
+
+ console.log("TESTING Part B calc() ---------------------------\n");
+
+ console.log(pc.calc('{"op" : "add", "number" : 5}'));          // 5
+//  console.log(calc('{"op" : "subtract", "number" : 2}'));     // 3
+//  console.log(calc('{"op" : "add", "number" : 19}'));         // 22
+//  // nested expressions
+//  console.log(calc('{"op": "subtract", "expr" : {"op" : "add", "number" : 15}}'));    // 0
+//  console.log(calc('{"op": "add", "expr" : {"op" : "add", "expr" : {"op" : "subtract", "number" : 3}}}')); // -12
+//  console.log(calc('{"op": "add", "expr" : {"op" : "add", "expr" : {"op" : "add", "number" : 6}}}')); // -24
+//  console.log(calc('{"op": "add", "expr" : {"op" : "subtract", "expr" : {"op" : "add", "expr" : {"op" : "subtract", "number" : 1}}}}')); // 0
+ 
+//  console.log(cleanup());
+ console.log("\n");
