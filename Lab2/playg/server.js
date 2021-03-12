964bd18d20b370ec3aeb87e1b615bf3702f02a12
akclifto <akclifto@asu.edu>
2021-03-11 18:08:16 -0700

    video example server

---
+++ Lab2/playg/server.js
@@ -0,0 +1,9 @@
+import http from 'http';
+
+http.createServer(function(req, res) { //this is an anon callback
+    res.writeHead(200, {"content-type": "application/json"});
+    res.end("Hello World!");
+
+}).listen(8080);
+
+console.log("Server started");
