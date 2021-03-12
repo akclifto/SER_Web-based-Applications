47e2c09d90549333a8a02a2a960c93a49fbde3c3
akclifto <akclifto@asu.edu>
2021-03-12 12:04:19 -0700

    http server get node http 2

---
+++ Lab2/playg/httpServerGet.js
@@ -0,0 +1,47 @@
+// Example from Brad Dayley
+// https://github.com/bwdbooks/nodejs-mongodb-angularjs-web-development
+
+import { createServer } from 'http';
+import { parse } from 'url';
+var messages = ['Hello World', 'From a Node.js server', 'Take Luck'];
+
+createServer(function (req, res) {
+    let resBody = '';
+    let resMsg = '';
+    // This way of parsing a query string is deprecated but I still find it much easier
+    // than trying to use the new WhatWG URL object and trying to parse the searchParams
+    let urlObj = parse(req.url, true, false); //deprecated
+    // const baseURL = "http://" + req.headers.host + "/";
+    // let urlObj = new URL(req.url, baseURL);
+    let qstr = urlObj.query;
+    console.log(qstr);
+    if (!qstr.msg) {
+        resMsg = '<h2>No msg parameter</h2>\n';
+    } else {
+        resMsg = '<h1>'+messages[qstr.msg]+'</h2>';
+    }
+    resBody = resBody + '<html><head><title>Simple HTTP Server</title></head>';
+    resBody = resBody + '<body>' + resMsg;
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(resBody + '\n</body></html>');
+}).listen(8088);
+
+/**
+ * Going to the page gives 'no msg parameter'. at localhost:8080
+ * 
+ * Try localhost:8080?msg=foo
+ *      gives 'undefined' message, because 'foo' isn't an index in var messages above.
+ * 
+ * Try localhost:8080?msg=0
+ *      gives 'Hello World'
+ * 
+ * Try localhost:8080?msg=1
+ *      gives 'From a Node.js server'
+ * 
+ * Try localhost:8080?msg=2
+ *      gives 'Take Luck'
+ * 
+ * Try localhost:8080?msg=3
+ *      gives 'undefined' again bc not valid index in var messages.
+ */
--- Lab2/playg/httpServerStatic.js
+++ Lab2/playg/httpServerStatic.js
@@ -5,10 +5,14 @@
 // serves html file in html folder in playg, change the path name above to see what the error looks like.
 import { readFile } from 'fs';
 import { createServer } from 'http';
-import { parse } from 'url';
+// import { parse } from 'url'; // parse deprecated
+
+// parse has been deprecated, use baseURL like below then urlObj = new URL() like below to get object info.
 var ROOT_DIR = "html/";
 createServer(function (req, res) {
-    var urlObj = parse(req.url, true, false);
+    // var urlObj = parse(req.url, true, false); // deprecated
+    const baseURL = 'http://' + req.headers.host + '/';
+    const urlObj = new URL(req.url, baseURL);
     console.log(urlObj);
   readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
     if (err) {
