53a61566a31f15803fea18d083e68a87ce5de80c
akclifto <akclifto@asu.edu>
2021-03-12 10:59:19 -0700

    static server ex node http 2

---
+++ Lab2/playg/httpServerStatic.js
@@ -0,0 +1,27 @@
+// Example from Brad Dayley
+// https://github.com/bwdbooks/nodejs-mongodb-angularjs-web-development
+
+// Access http://localhost:8080/hello.html
+// serves html file in html folder in playg, change the path name above to see what the error looks like.
+import { readFile } from 'fs';
+import { createServer } from 'http';
+import { parse } from 'url';
+var ROOT_DIR = "html/";
+createServer(function (req, res) {
+    var urlObj = parse(req.url, true, false);
+    console.log(urlObj);
+  readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200, {
+	'Content-Type': 'text/html',
+        'Set-Cookie': 'foo=bar'
+    });
+    res.end(data);
+  });
+}).listen(8080, 'localhost', 3, function() { 
+                console.log('I am now ready!'); 
+});
