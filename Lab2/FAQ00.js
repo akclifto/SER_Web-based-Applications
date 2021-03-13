9469b8fcb8e49f588f5686aa9c47ed67986f5b93
akclifto <akclifto@asu.edu>
2021-03-12 23:07:28 -0700

    get id function, work on write qa

--- Lab2/FAQ00.js
+++ Lab2/FAQ00.js
@@ -7,7 +7,7 @@
  * 
  */
 import path from "path";
-import { readFileSync, read } from 'fs';
+import { readFileSync, readFile } from 'fs';
 import QA from "./QA.js";
 
 const __dirname = path.resolve();
@@ -17,17 +17,9 @@ class FAQ {
 
     constructor(jsonFile) {
         this.dataStore = this.loadQA(jsonFile);
-        console.log(this.dataStore);
     }
 
-    /**
-     * Async Attempt to read file.
-     * @param {*} jsonFile 
-     * @returns 
-     */
-    loadTest(jsonFile) {
 
-    }
 
     /**
      * LoadQA loads data from json file.
@@ -35,8 +27,8 @@ class FAQ {
      * @returns parsed json data for local storage
      */
     loadQA(jsonFile) {
-    
-        const options = {encoding:'utf8', flag:'r'};
+
+        const options = { encoding: 'utf8', flag: 'r' };
         let qas = readFileSync(jsonFile, options);
         try {
             return JSON.parse(qas);
@@ -44,15 +36,27 @@ class FAQ {
             console.log("Error in loadQA: ", err);
         }
     }
+    
+    /**
+     * Method to generate random id for new QAs
+     * @returns random float id
+     */
+    getId() {
+        let left = Math.floor(Math.random() * 1000000).toString();
+        let right = Math.floor(Math.random() * 10000).toString();
+        let genID = left.concat(".").concat(right);
+        // console.log(parseFloat(str).toFixed(4));
+        return genID;
+    }
 }
 
 
-
-
 FAQ.prototype.writeQA = function (question, answer, author, date, tags) {
-    
+
     //TODO: 
     // R1. The ability to write a Q&A to the persistent store (sample JSON of the Q&A is provided)
+    let qa = new QA(question, answer, author, date, tags);
+
 }
 FAQ.prototype.updateQA = function (id, answer) {
     //TODO:
@@ -66,7 +70,7 @@ FAQ.prototype.deleteQA = function (id) {
     //TODO:
     // R4. The ability to delete a Q&A from an existing persistent store.
 }
-FAQ.prototype.filter = function(options) {
+FAQ.prototype.filter = function (options) {
     //TODO
     /* R5. The ability to return a collection of Q&As based on a filter, where the filter checks for one or
         more of the criteria such as:
@@ -77,5 +81,24 @@ FAQ.prototype.filter = function(options) {
 }
 
 
-new FAQ(__dirname + "/QA.json");
 
+const faq = new FAQ(__dirname + "/QA.json");
+// console.log(faq.dataStore);
+
+
+    // /**
+    //  * Async Attempt to read file async.
+    //  * @param {*} jsonFile 
+    //  * @returns nothing, doesn't work
+    //  */
+    // async loadTest(jsonFile) {
+    //     const options = { encoding: 'utf8', flag: 'r' };
+    //     readFile(jsonFile, options, function (err, data) {
+    //         if (err) {
+    //             console.log("Error in loadTest: ", err);
+    //         } else {
+    //             let qas = JSON.parse(data);
+    //             return qas;
+    //         }
+    //     });
+    // }
