2cb8ce4ae925d0811b0908ebf6efe77df7a7bd87
akclifto <akclifto@asu.edu>
2021-03-26 15:13:33 -0700

    start on the first part of activity 1

--- Lab3/routes/index.js
+++ Lab3/routes/index.js
@@ -1,10 +1,34 @@
 /* eslint-disable no-unused-vars */
-var express = require("express");
-var router = express.Router();
+let express = require("express");
+let fs = require("fs");
+let path = require("path");
+let router = express.Router();
+const FILE_DIR = path.resolve();
+console.log(FILE_DIR);
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "akclifto_Lab3", subTitle: "Express" });
+  fs.readFile(
+    FILE_DIR.concat("/resource/article.txt"),
+    "UTF8",
+    function (err, data) {
+      if (err) {
+        console.log("readFile error: ", err);
+      }
+
+      let article = data;
+      // console.log(article);
+
+      let title = "Sample of Article: ";
+
+      //render the article to articleBody
+      res.render("index", {
+        title: "Welcome to the Article Reviewer App",
+        articleTitle: title,
+        articleBody: JSON.stringify(article),
+      });
+    }
+  );
 });
 
 module.exports = router;
