922a7ad83ffb26c441ae6a56e3442ad44f441160
akclifto <akclifto@asu.edu>
2021-03-27 12:34:53 -0700

    update index design, test readfile callback

--- Lab3/routes/index.js
+++ Lab3/routes/index.js
@@ -3,32 +3,31 @@ let express = require("express");
 let fs = require("fs");
 let path = require("path");
 let router = express.Router();
+
 const FILE_DIR = path.resolve();
-console.log(FILE_DIR);
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
+  let article = "";
+
   fs.readFile(
-    FILE_DIR.concat("/resource/article.txt"),
-    "UTF8",
-    function (err, data) {
+    FILE_DIR.concat("/resource/article.txt"), "UTF8", function (err, data) {
       if (err) {
         console.log("readFile error: ", err);
       }
-
-      let article = data;
+      // article = data;
       // console.log(article);
-
-      let title = "Sample of Article: ";
-
-      //render the article to articleBody
-      res.render("index", {
-        title: "Welcome to the Article Reviewer App",
-        articleTitle: title,
-        articleBody: article.toString(),
-      });
+      return data;
     }
   );
+  
+  let title = "Sample of Article: ";
+  //render the article to articleBody
+  res.render("index", {
+    title: "Welcome to the Article Reviewer App",
+    articleTitle: title,
+    articleBody: article.toString(),
+  });
 });
 
 module.exports = router;
