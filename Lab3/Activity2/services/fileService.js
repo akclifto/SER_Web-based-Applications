568bc9a5798da208a90db205a48dfbae8357225c
akclifto <akclifto@asu.edu>
2021-03-30 15:57:41 -0700

    refactor out fileService

---
+++ Lab3/Activity2/services/fileService.js
@@ -0,0 +1,72 @@
+const fs = require("fs");
+
+const paths = require("./constants");
+const logger = require("../services/log");
+
+/**
+ * File Service to manage reading and writing files.
+ */
+
+/**
+ * Method to write data to file.
+ * @param {*} res : server response
+ * @param {*} file : file to write
+ * @param {*} data : data to write to file
+ */
+function writeToFile(res, file, data) {
+  try {
+    fs.writeFileSync(
+      paths.FILE_DIR.concat(file),
+      JSON.stringify(data),
+      "UTF8",
+      function (err) {
+        if (err) {
+          let error = logger.setErrorMessage(500, err.message);
+          res.render("error", { error });
+        }
+      }
+    );
+  } catch (err) {
+    logger.errorLog("WriteToFile", err);
+  }
+}
+
+/**
+ * Method to get questions. Reads question file from resource folder.
+ * @param {*} res : server response
+ * @returns parsed json questions.
+ */
+function getQuestions(res) {
+  return new Promise(function (resolve, reject) {
+    try {
+      fs.readFile(
+        paths.FILE_DIR.concat(paths.QUESTIONS_JSON),
+        "UTF8",
+        function (err, data) {
+          if (err) {
+            logger.errorLog("readFile", err);
+            let error = logger.setErrorMessage(500, err.message);
+            // render the pug error template
+            reject(res.render("error", { error }));
+          }
+
+          let questions = JSON.parse(data);
+          if (questions.length === 0 || questions === "") {
+            // set an initial, blank comment.
+            resolve("empty");
+          } else {
+            // console.log(data);
+            resolve(questions);
+          }
+        }
+      );
+    } catch (err) {
+      logger.errorLog("getArticle error: ", err);
+    }
+  });
+}
+
+module.exports = {
+  writeToFile,
+  getQuestions,
+};
