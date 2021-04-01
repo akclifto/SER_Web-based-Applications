f420ef229980db533472d179d2a81698c05ce38b
akclifto <akclifto@asu.edu>
2021-04-01 13:15:56 -0700

    update login cred per act 3 R2

--- Lab3/Activity2/middleware/authenticate.js
+++ Lab3/Activity2/middleware/authenticate.js
@@ -1,7 +1,7 @@
-let fakeDB = require("../db/fakedb");
+// let fakeDB = require("../db/fakedb");
 
 const logger = require("../services/log");
-fakeDB = new fakeDB();
+// fakeDB = new fakeDB();
 
 /**
  * Method to authenticate admin login via fake database query.
@@ -12,20 +12,24 @@ fakeDB = new fakeDB();
 function authenticate(username, password) {
   //   console.log(fakeDB.db);
   return new Promise(function (resolve, reject) {
-    try {
-      fakeDB.db.forEach(function (item) {
-        // console.log(username + " " + password);
-        // console.log(item.username + " " + item.password);
-        if (username === item.username && password === item.password) {
-          logger.serverLog(`User/password match found for ${username}`);
-          resolve(true);
-        }
-      });
-    } catch (err) {
-      logger.errorLog("authenticate", err);
+    // try {
+    //   fakeDB.db.forEach(function (item) {
+    //     // console.log(username + " " + password);
+    //     // console.log(item.username + " " + item.password);
+    //     if (username === item.username && password === item.password) {
+    //       logger.serverLog(`User/password match found for ${username}`);
+    //       resolve(true);
+    //     }
+    //   });
+    // } catch (err) {
+    //   logger.errorLog("authenticate", err);
+    // }
+    if (username === password) {
+      resolve(true);
+    } else {
+      logger.serverLog(`No User/password found for ${username}`);
+      reject(false);
     }
-    logger.serverLog(`No User/password found for ${username}`);
-    reject(false);
   });
 }
 
