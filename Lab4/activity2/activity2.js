8fd48909b145bfa17a3cc6838334f5e036714864
akclifto <akclifto@asu.edu>
2021-04-05 18:28:59 -0700

    Work on r1-r2, cookies don't work

--- Lab4/activity2/activity2.js
+++ Lab4/activity2/activity2.js
@@ -5,7 +5,7 @@
  *
  */
 
-function getUsername() {
+function handleUsername() {
   let username = document.getElementById("username").value.trim();
   if (username.length > 0 && username !== " ") {
     setCookie(username);
@@ -42,10 +42,89 @@ function getCookie(cname) {
 function displayGreeting(username) {
   // TODO cookies don't work,
   //if return "Welcome back <username>! Please enter your comments about the movie."
-  let greeting = `Hello ${username}. Welcome to movie review System! Please enter your comments about the movie.`;
+  let greeting = `Hello ${username}.  Welcome to movie review System!  Please enter your comments about the movie.`;
   document.getElementById("welcome").innerHTML = greeting;
 }
 
 function setReviewContent() {
-
+  document.getElementById("review-movie").innerHTML = critic.movie;
+  document.getElementById("review-body").innerHTML = critic.review;
 }
+
+let dict = {
+  dictionary_name: "default",
+  entries: [
+    {
+      key: [
+        "stupid",
+        "dumb",
+        "idiot",
+        "unintelligent",
+        "simple-minded",
+        "braindead",
+        "foolish",
+        "unthoughtful",
+      ],
+      answer: ["educated", "informed", "schooled"],
+    },
+    {
+      key: ["unattractive", "hideous", "ugly"],
+      answer: [
+        "attractive",
+        "beauteous",
+        "beautiful",
+        "lovely",
+        "pretty",
+        "ravishing",
+      ],
+    },
+    {
+      key: [
+        "ambiguous",
+        "cryptic",
+        "dark",
+        "nebulous",
+        "obscure",
+        "unintelligible",
+      ],
+      answer: [
+        "obvious",
+        "plain",
+        "unambiguous",
+        "understandable",
+        "unequivocal",
+      ],
+    },
+    {
+      key: [
+        "incapable",
+        "incompetent",
+        "inept",
+        "unable",
+        "unfit",
+        "unqualified",
+        "weak",
+        "artless",
+      ],
+      answer: ["accomplished", "fit", "adept", "complete", "consummate"],
+    },
+    {
+      key: ["emotionless", "heartless", "unkind", "mean", "selfish", "evil"],
+      answer: ["benevolent", "benignant", "gentle", "kind", "clement"],
+    },
+    {
+      key: ["idle"],
+      answer: [
+        "Can you reply something?",
+        "You have been idle for more than 30 seconds",
+        "Whats the matter with you? Submit something",
+      ],
+    },
+  ],
+};
+
+let critic = {
+  movie: "The Room (2003)",
+  review:
+    "I've never in my life been more entertained by a film that has absolutely NO redeeming qualities.  The sense of alienation emanating from this film places the audience extremely far from being able to relate to what's happening on screen which leaves a lot of room for uncontrollable laughter given the right circumstances. The camera work and production techniques would not be out of place in many daytime soap operas, nor would the script and plot, but there is an undefinable quality which separates this movie from the sense mediocrity often found in such shows and instead casts it deep into the abyss of tragically bad film making where it will be forever trapped along with Wiseau's artistic integrity. This really is a new frontier. It is truly awful, but I cannot recommend it enough.",
+};
