2ed4632bbac70b66d730bd5f56c4683ab01b9f1e
akclifto <akclifto@asu.edu>
2021-04-12 00:31:33 -0700

    add 4040 page

--- Lab5/activity1/app.js
+++ Lab5/activity1/app.js
@@ -15,4 +15,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 
+app.use((req, res, next) => {
+  res.send("404: Page Not Found <br><br><a href='/'> Go Home</a>", 404);
+});
+
 module.exports = app;
