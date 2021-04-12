96596bd6ebdc13d3aa61d4b8b2504fb6c14c3724
akclifto <akclifto@asu.edu>
2021-04-12 10:00:43 -0700

    work on conversions

--- Lab5/activity1/public/javascripts/activity1.js
+++ Lab5/activity1/public/javascripts/activity1.js
@@ -5,23 +5,53 @@
  * @email akclifto@asu.edu
  */
 
+function checkInput() {
+  let input = document.getElementById("currency-usd").value;
+  let conv = "";
+  try {
+    conv = parseFloat(input).toFixed(2);
+  } catch (err) {
+    console.log(err.message);
+  }
+  if (conv !== "" && conv > 0) {
+    document.getElementById("euro").removeAttribute("disabled");
+    document.getElementById("pound").removeAttribute("disabled");
+  } else {
+    document.getElementById("euro").setAttribute("disabled", "disabled");
+    document.getElementById("pound").setAttribute("disabled", "disabled");
+  }
+}
+/**
+ * Method to convert USD to EURO
+ */
 function convertEuro() {
   console.log("convert euro clicked");
-
 }
 
+/**
+ * Method to convert USD to GBP
+ */
 function convertGBP() {
   console.log("convert GBP clicked");
 }
 
+/**
+ * Method to pop last action from history.
+ */
 function pop() {
   console.log("pop clicked");
 }
 
+/**
+ * Method to reset history activity.
+ */
 function reset() {
   console.log("reset clicked");
 }
 
+/**
+ * Method to display user activity.
+ */
 function showHistory() {
   console.log("show history clicked");
 }
