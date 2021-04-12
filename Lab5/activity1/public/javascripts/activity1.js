17be746003ea95efcd214633645bf0b0772e0991
akclifto <akclifto@asu.edu>
2021-04-12 00:06:08 -0700

    update html, start client js functions

--- Lab5/activity1/public/javascripts/activity1.js
+++ Lab5/activity1/public/javascripts/activity1.js
@@ -1,3 +1,9 @@
+/* eslint-disable no-unused-vars */
+/**
+ * Lab 5 Activity 1
+ * @author Adam Clifton
+ * @email akclifto@asu.edu
+ */
 function showHistory() {
   console.log("show history clicked");
 }
---
+++ Lab5/activity1/readme.txt
@@ -0,0 +1,7 @@
+/**
+ * Lab 5 Activity 1
+ * @author Adam Clifton
+ * @email akclifto@asu.edu
+ */
+
+Lab 5 Activity 1 API documentation:
